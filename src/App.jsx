91437bcbab3fa68c167b1ecc2e111ed2f033cc82
Yurii Cherniak <juracherniak@gmail.com>
2024-04-13 23:28:23 +0200

    Add filtering

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,32 @@ import { useState, useEffect } from 'react'
 import Header, { modalStyle } from './components/Header.jsx'
 import './App.css'
 import SignIn from './components/SignIn.jsx';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
 import ListItem from './components/ListItem.jsx';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { getFiles } from './firebase.js';
 
-const mockData = [
-    { title: "Plagiátorstvo", user: "JanAnonimus", likes: 12, filename: "plagiat.pdf", url: "hhttps://firebasestorage.googleapis.com/v0/b/hip-babic.appspot.com/o/SVT-1.pdf?alt=media&token=3d198b01-36ba-48fc-8824-f3e2b8c7f4c1ttps://firebasestorage.googleapis.com/v0/b/hip-babic.appspot.com/o/SVT-1.pdf?alt=media&token=3d198b01-36ba-48fc-8824-f3e2b8c7f4c1" }, { title: "Vývoj umelé inteligencie", user: "JanAnonimus", likes: 12, filename: "plagiat.pdf" }, { title: "Vývoj umelé inteligencie", user: "JanAnonimus", likes: 12, filename: "plagiat.pdf" }, { title: "Vývoj umelé inteligencie", user: "JanAnonimus", likes: 12, filename: "plagiat.pdf" }, { title: "Vývoj umelé inteligencie", user: "JanAnonimus", likes: 12, filename: "plagiat.pdf" }, { title: "Vývoj umelé inteligencie", user: "JanAnonimus", likes: 12, filename: "plagiat.pdf" }
-]
+export const univerzity = [{ value: 1, label: "TUKE" }, { value: 2, label: "UPJS" }]
+export const fakulty = [{ value: 1, label: "FEI", univerzita: 1 }, { value: 2, label: "SjF", univerzita: 1 }, { value: 3, label: "LF", univerzita: 2 }]
+export const predmety = [
+    { value: 1, label: "RIP", univerzita: 1, fakulta: 1 },
+    { value: 2, label: "SVT", univerzita: 1, fakulta: 1 },
+    { value: 3, label: "HIP", univerzita: 1, fakulta: 1 },
+    { value: 4, label: "KMA", univerzita: 1, fakulta: 2 },
+    { value: 5, label: "KMA", univerzita: 2, fakulta: 3 }]
+
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [data, setData] = useState([]);
     const [originaldata, setoriginalData] = useState([]);
     const [searchText, setSearchText] = useState("");
+    const [univerzita, setUniverzita] = useState(null);
+    const [fakulta, setFakulta] = useState(null);
+    const [predmet, setPredmet] = useState(null);
 
     useEffect(() => {
         getFiles().then(files => {
@@ -40,6 +48,15 @@ function App() {
         setData(originaldata.filter(item => item.title.toLowerCase().includes(searchText.toLowerCase())));
     }, [searchText])
 
+    useEffect(() => {
+        setFakulta(null);
+        setPredmet(null);
+    }, [univerzita])
+
+    useEffect(() => {
+        setPredmet(null);
+    }, [fakulta])
+
     return (
         <>
             <Header refreshData={refreshData} isLoggedIn={isLoggedIn} setLogin={setIsLoggedIn} />
@@ -47,7 +64,7 @@ function App() {
                 {!isLoggedIn ? <SignIn setIsLoggedIn={setIsLoggedIn} /> :
                     <>
                         <div className='flex column' style={{ alignItems: "start", gap: "30px" }}>
-                            <Breadcrumbs aria-label="breadcrumb" style={{ fontSize: "22px" }}>
+                            {/* <Breadcrumbs aria-label="breadcrumb" style={{ fontSize: "22px" }}>
                                 <Link underline="hover" color="black" href="/">
                                     TUKE
                                 </Link>
@@ -76,11 +93,57 @@ function App() {
                                     SVT
                                 </Link>
 
-                            </Breadcrumbs>
+                            </Breadcrumbs> */}
+                            <div className='flex' style={{ width: "100%", gap: "10px" }}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="univerzita-label">University</InputLabel>
+                                    <Select
+                                        labelId="univerzita-label"
+                                        value={univerzita}
+                                        label="Univerzita"
+                                        onChange={(e) => setUniverzita(e.target.value)}
+                                    >
+                                        <MenuItem value={null}> </MenuItem>
+                                        {univerzity.map((item) => (
+                                            <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+
+                                <FormControl fullWidth>
+                                    <InputLabel id="fakulta-label">Faculty</InputLabel>
+                                    <Select
+                                        labelId="fakulta-label"
+                                        value={fakulta}
+                                        label="Fakulta"
+                                        onChange={(e) => setFakulta(e.target.value)}
+                                    >
+                                        <MenuItem value={null}> </MenuItem>
+                                        {fakulty.filter((f) => f.univerzita === univerzita).map((item) => (
+                                            <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <FormControl fullWidth>
+                                    <InputLabel id="Predmet-label">Class</InputLabel>
+                                    <Select
+                                        labelId="Predmet-label"
+                                        value={predmet}
+                                        label="Predmet"
+                                        onChange={(e) => setPredmet(e.target.value)}
+                                    >
+                                        <MenuItem value={null}>   </MenuItem>
+                                        {predmety.filter((p) => p.univerzita === univerzita && p.fakulta === fakulta).map((item) => (
+                                            <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </div>
                             <TextField
                                 value={searchText}
                                 onChange={(e) => setSearchText(e.target.value)}
                                 label="Search"
+                                fullWidth
                                 InputProps={{
                                     endAdornment: (
                                         <InputAdornment>
@@ -91,13 +154,15 @@ function App() {
                                     )
                                 }}
                             />
-                            <Typography variant="h4" style={{ color: "black", fontWeight: "bold" }}>Spolecenske vedy a technika</Typography>
+                            <Typography variant="h4" style={{ color: "black", fontWeight: "bold" }}>Materials</Typography>
 
                             <div style={{ width: "100%" }}>
                                 {data.length === 0 && <Typography variant="h5" style={{ color: "black" }}>No files found</Typography>}
-                                {data.map((item, index) => (
-                                    <ListItem refreshData={refreshData} key={index} item={item} />
-                                ))}
+                                {data.filter(value => (!univerzita || value.univerzita === univerzita)
+                                    && (!fakulta || value.fakulta === fakulta)
+                                    && (!predmet || value.predmet === predmet)).map((item, index) => (
+                                        <ListItem refreshData={refreshData} key={index} item={item} />
+                                    ))}
                             </div>
                         </div>
                     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
@@ -6,6 +6,9 @@ import Box from '@mui/material/Box';
 import CloseIcon from '@mui/icons-material/Close';
 import { uploadFile } from '../firebase';
 import { useDropzone } from 'react-dropzone';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { fakulty, predmety, univerzity } from '../App';
 
 export const modalStyle = {
     position: 'absolute',
@@ -21,6 +24,10 @@ export const modalStyle = {
 
 export default function Header(props) {
     const [open, setOpen] = React.useState(false);
+    const [univerzita, setUniverzita] = useState(null);
+    const [fakulta, setFakulta] = useState(null);
+    const [predmet, setPredmet] = useState(null);
+    const [file, setFile] = useState(null);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
@@ -34,11 +41,16 @@ export default function Header(props) {
 
         if (!file) return;
 
-        uploadFile(file).then(() => {
+        setFile(file);
+    };
+
+    const handleFileUpload = () => {
+        uploadFile(file, univerzita, fakulta, predmet).then(() => {
             handleClose();
             props.refreshData();
         });
-    };
+    }
+
 
     return (
         <>
@@ -58,6 +70,7 @@ export default function Header(props) {
                         >
                             + Add material
                         </Button>
+                        <Typography variant="h6" style={{ margin: "0px", display: "flex", justifyContent: "center", alignItems: "center" }}><AccountBalanceWalletIcon /> Balance: 0.5$</Typography>
                         <Typography variant="h6" style={{ margin: "0px" }}>Tomas Zverbik</Typography>
                         <img src="https://s3-alpha-sig.figma.com/img/60cc/08a6/58479468419501ed066f00d83aefa417?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=AyA7mpjKouxnylCEdmsttAaYMHbmhqq2Du2Mh8GT~7NYOHg4GPhbJkDXHjlXl4-tQ8t~sWB46HgogAd8WlYmkoqUVci1nnDhc01w4YPAmAmy9DlsuCukUUM3ZJ3GY71A6oEVqU4-8nTSQtuxAzwFZ0RlH28moybYQBU~czCKxwKsgNBcKk0ld1U9MMPZNpuk5RBq2EiUBcnA0rixI2NF~rq2jI3X976PnNk6Klx9kkLC3YNeACOnTv9JIAnaiVDZvrzXf~6jUOBnK4JMIXqXmufgpNrpIhdRnEGqITBOuDrCGy-1C2TOVwG8cui5YaoQjeydCccfMju7~nOd9TRfSA__" alt="Tomas Zverbik" width="40" height="40" style={{ borderRadius: "50%", borderColor: "black", border: "1px solid" }} />
                     </div>}
@@ -83,10 +96,58 @@ export default function Header(props) {
                         <CloseIcon style={{ position: "absolute", right: "10px", top: "10px", cursor: "pointer" }} fontSize='large' onClick={handleClose} />
                     </Typography>
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Choose the type of upload you want to do for your files
+                        Select university, faculty and class
                     </Typography>
-                    <Basic handleFileChange={handleFileChange}/>
+                    <div className='flex' style={{ width: "100%", gap: "10px", marginTop: "20px" }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="univerzita-label">University</InputLabel>
+                            <Select
+                                labelId="univerzita-label"
+                                value={univerzita}
+                                label="Univerzita"
+                                onChange={(e) => setUniverzita(e.target.value)}
+                            >
+                                <MenuItem value={null}> </MenuItem>
+                                {univerzity.map((item) => (
+                                    <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+
+                        <FormControl fullWidth>
+                            <InputLabel id="fakulta-label">Faculty</InputLabel>
+                            <Select
+                                labelId="fakulta-label"
+                                value={fakulta}
+                                label="Fakulta"
+                                onChange={(e) => setFakulta(e.target.value)}
+                            >
+                                <MenuItem value={null}> </MenuItem>
+                                {fakulty.filter((f) => f.univerzita === univerzita).map((item) => (
+                                    <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl fullWidth>
+                            <InputLabel id="Predmet-label">Class</InputLabel>
+                            <Select
+                                labelId="Predmet-label"
+                                value={predmet}
+                                label="Predmet"
+                                onChange={(e) => setPredmet(e.target.value)}
+                            >
+                                <MenuItem value={null}>   </MenuItem>
+                                {predmety.filter((p) => p.univerzita === univerzita && p.fakulta === fakulta).map((item) => (
+                                    <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <Basic handleFileChange={handleFileChange} filename={file?.name || ""} />
                     {/* <input id='fileupload' type="file" onChange={handleFileChange} style={{ display: "flex" }} /> */}
+                    <div style={{ display: "flex", justifyContent: "end" }}>
+                        <Button disabled={!univerzita || !fakulta || !predmet || !file} variant="contained" sx={{ bgcolor: "black", marginTop: "20px" }} onClick={handleFileUpload}>Upload</Button>
+                    </div>
                 </Box>
             </Modal>
         </>
@@ -96,21 +157,17 @@ export default function Header(props) {
 function Basic(props) {
     const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
-    const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-            {file.path} - {file.size} bytes
-        </li>
-    ));
-
     return (
         <section className="container">
             <div {...getRootProps({ className: 'dropzone' })}>
-                <input {...getInputProps()} onChange={props.handleFileChange}/>
+                <input {...getInputProps()} onChange={props.handleFileChange} />
                 <p>Drag 'n' drop some files here, or click to select files</p>
             </div>
-            {acceptedFiles.length > 0 && <aside>
-                <h4>Files</h4>
-                <ul>{files}</ul>
+            {props.filename && <aside>
+                <h4>File</h4>
+                <ul style={{ listStyleType: "none", padding: "0px" }}>
+                    <li>{props.filename}</li>
+                </ul>
             </aside>
             }
         </section>
